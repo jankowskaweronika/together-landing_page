cb7ce900807d3bd60901779e37c2904090646ac0
Weronika Jankowska <wjankowska8@gmail.com>
2025-12-02 20:58:49 +0100

    Changed metadata titles and descriptions, updated site header logo, and modified hero section text.

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -17,16 +17,14 @@ export default function HeroSection() {
       className="relative mx-auto mt-32 max-w-[80rem] px-6 text-center md:px-8"
     >
       <h1 className="bg-gradient-to-br dark:from-white from-black from-30% dark:to-white/40 to-black/40 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent text-balance sm:text-6xl md:text-7xl lg:text-8xl translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
-        Magic UI is the new way
-        <br className="hidden md:block" /> to build landing pages.
+        <br className="hidden md:block" /> Wspólna droga do wiedzy
       </h1>
       <p className="mb-12 text-lg tracking-tight text-gray-400 md:text-xl text-balance translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
-        Beautifully designed, animated components and templates built with
-        <br className="hidden md:block" /> Tailwind CSS, React, and Framer
-        Motion.
+        Ucz się, dyskutuj, rozwijaj
+        <br className="hidden md:block" /> - wszystko w Together
       </p>
       <Button className="translate-y-[-1rem] animate-fade-in gap-1 rounded-lg text-white dark:text-black opacity-0 ease-in-out [--animation-delay:600ms]">
-        <span>Get Started for free </span>
+        <span>wypróbuj za darmo </span>
         <ArrowRightIcon className="ml-1 size-4 transition-transform duration-300 ease-in-out group-hover:translate-x-1" />
       </Button>
       <div
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -6,6 +6,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import { AlignJustify, XIcon } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import Image from "next/together.png"
 
 const menuItem = [
   {
@@ -99,9 +100,12 @@ export function SiteHeader() {
     <>
       <header className="fixed left-0 top-0 z-50 w-full px-4 animate-fade-in border-b opacity-0 backdrop-blur-[12px] [--animation-delay:600ms]">
         <div className="container mx-auto flex h-[var(--navigation-height)] w-full items-center justify-between">
-          <Link className="text-md flex items-center justify-center" href="/">
-            Magic UI
-          </Link>
+          <img
+            src="/logo.png"
+            alt="together-course-logo"
+            width={250}
+            height={300}
+          />
 
           <div className="ml-auto flex h-full items-center">
             <Link className="mr-6 text-sm" href="/signin">
@@ -165,9 +169,8 @@ export function SiteHeader() {
                 className="border-grey-dark pl-6 py-0.5 border-b md:border-none"
               >
                 <Link
-                  className={`hover:text-grey flex h-[var(--navigation-height)] w-full items-center text-xl transition-[color,transform] duration-300 md:translate-y-0 md:text-sm md:transition-colors ${
-                    hamburgerMenuIsOpen ? "[&_a]:translate-y-0" : ""
-                  }`}
+                  className={`hover:text-grey flex h-[var(--navigation-height)] w-full items-center text-xl transition-[color,transform] duration-300 md:translate-y-0 md:text-sm md:transition-colors ${hamburgerMenuIsOpen ? "[&_a]:translate-y-0" : ""
+                    }`}
                   href={item.href}
                 >
                   {item.label}
