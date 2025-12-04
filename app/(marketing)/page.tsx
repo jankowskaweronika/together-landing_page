e45fac11fd650c2c715a1bdef348d87501846283
Weronika Jankowska <wjankowska8@gmail.com>
2025-12-04 20:23:49 +0100

    Add animated BorderBeam component for enhanced visual effects and update marketing page layout with additional SphereMask

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -14,6 +14,7 @@ export default async function Page() {
       <BenefitsSection />
       <SphereMask />
       <ComparisonSection />
+      <SphereMask reverse={true} />
       <FeaturesSection />
       <HowItWorksSection />
       <ContactSection />
