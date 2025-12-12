7922e57380d055b3640173b99aa000c2861bd8b8
Weronika Jankowska <wjankowska8@gmail.com>
2025-12-12 14:30:07 +0100

    🗑️ Remove internationalization support and related configurations, including language files and middleware.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
-const withNextIntl = require('next-intl/plugin')();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = withNextIntl(nextConfig);
+export default nextConfig;
